077f96fb665497428d276150b49235176bd796c5
Kyle Dyroff <kyle.dyroff@gmail.com>
2020-02-13 23:48:27 -0500

    Add initial swagger/redoc documentation

--- househunter/househunter/urls.py
+++ househunter/househunter/urls.py
@@ -15,15 +15,29 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url, include
-from rest_framework import routers
+from rest_framework import permissions, routers
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from houses import views
 
 router = routers.DefaultRouter()
 router.register(r'houses', views.HouseViewSet)
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="HouseHunter API",
+      default_version='v1',
+      description="HouseHunter demo API",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
